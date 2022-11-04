45f065c3faa74a43516989569581a85a67863123
Priyanka Pansuriya <99384120+Priyankapansuriya@users.noreply.github.com>
2022-11-04 17:28:34 +0530

    Updated code

---
+++ src/component/Menu.js
@@ -0,0 +1,68 @@
+import { View, Text, TouchableOpacity, StyleSheet,Image} from 'react-native';
+import {useNavigation} from "@react-navigation/native"
+import React from 'react'
+
+const Menu = () => {
+
+const navigation = useNavigation();    
+  return (
+    <View style={styles.menuContainer}>
+    <TouchableOpacity style={styles.buttonStyle}
+     onPress={()=> navigation.navigate('About')}> 
+     <Image 
+         style = {styles.iconStyle} 
+         source={{
+          uri:"https://img.icons8.com/nolan/344/about.png"
+          }}
+        />
+    </TouchableOpacity> 
+
+    <TouchableOpacity style={styles.buttonStyle}
+     onPress={()=> navigation.navigate('Contact')}>
+     <Image
+          style={styles.iconStyle}
+          source={{
+            uri: "https://cdn2.iconfinder.com/data/icons/scenarium-vol-1-2/128/021_team_work_contacts_social_friends_messages_workplace-512.png",
+          }}
+        />
+    </TouchableOpacity> 
+
+    <TouchableOpacity style={styles.buttonStyle}
+     onPress={()=> navigation.navigate('Course')}>
+     <Image
+          style={styles.iconStyle}
+          source={{
+            uri: "https://img.icons8.com/external-photo3ideastudio-gradient-photo3ideastudio/344/external-certificate-online-learning-photo3ideastudio-gradient-photo3ideastudio.png",
+          }}
+        /> 
+    </TouchableOpacity> 
+
+    <TouchableOpacity style={styles.buttonStyle}
+     onPress={()=> navigation.navigate('User')}>
+     <Image
+          style={styles.iconStyle}
+          source={{
+            uri: "https://img.icons8.com/dusk/344/circled-user-male-skin-type-7.png",
+          }}
+        />
+    </TouchableOpacity>  
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    menuContainer: {
+        flexDirection: "row",
+        justifyContent: "space-evenly", 
+    },
+    textStyle: {
+        textTransform: "uppercase"
+    },
+    iconStyle: {
+       width:"100%",
+       height:50,
+       aspectRatio: 1, 
+    }
+})
+
+export default Menu;
---
+++ src/screens/Contact.js
@@ -0,0 +1,178 @@
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View ,Button} from 'react-native'
+import React, { useState } from 'react'
+import { WorkSans_400Regular } from '@expo-google-fonts/work-sans'
+import { Nunito_700Bold } from '@expo-google-fonts/nunito'
+import AppLoading from 'expo-app-loading'
+import { useFonts } from '@expo-google-fonts/work-sans'
+import Checkbox from 'expo-checkbox';
+
+const Contact = ({navigation}) => {
+
+let [fontsLoaded] = useFonts({
+  Nunito_700Bold ,
+  WorkSans_400Regular 
+})
+if (!fontsLoaded) {
+  <AppLoading />
+}
+
+const [name , setName] = useState("");
+const [email , setEmail] = useState("");
+const [num , setNum] = useState("");
+const [msg , setMsg] = useState("");
+const [agree , setAgree] = useState(false);
+
+const submit = () => {
+  if (!name && !email && !num && !msg) {
+    Alert.alert("Invalid Input","Plzz fill all the fields",
+    [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      }
+    ])
+    } else {
+      Alert.alert("Yehh",`Thank You ${name}`,
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ])
+      navigation.navigate("Home");
+    }
+  }
+
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.mainHeader}>Level up your knowledge</Text>
+      <Text style={styles.description}>You can reach us anytime</Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter Your Name</Text>
+        <TextInput style={styles.inputStyle}
+          placeholder={"Steve"}
+          value={name}
+          onChangeText={(userData)=>setName(userData)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter Your Email</Text>
+        <TextInput style={styles.inputStyle}
+          placeholder={"demo@gmail.com"}
+          value={email}
+          onChangeText={(email)=> setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter Your Mobile Number</Text>
+        <TextInput style={styles.inputStyle}
+          placeholder={"12345"}
+          value={num}
+          onChangeText={(num)=>setNum(num)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter Your Message</Text>
+        <TextInput style={[styles.inputStyle,styles.multiineStyle]}
+          placeholder={"About You"} 
+          value={msg}
+          onChangeText={(msg)=>setMsg(msg)}
+          numberOfLines={5}
+          multiline={true}
+        />
+      </View>
+    
+    <View style={styles.wrapper}>
+      <Checkbox 
+        value={agree}
+        onValueChange={()=> setAgree(!agree)}
+        color={agree ? "#4630EB" : undefined}
+      />
+      <Text style={styles.wrapperText}>I have read and agreed with TC</Text>
+    </View>
+
+    <TouchableOpacity style={[styles.buttonStyle,
+    {backgroundColor: agree ? "#4630EB" : "grey"} 
+    ]}
+    disabled={!agree}
+    onPress={submit}
+    >
+      <Text style={styles.buttonText}>Contact Us</Text>
+    </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: "100%",
+    paddingHorizontal: 30,
+    backgroundColor: "#fff",
+  },
+  mainHeader: {
+    fontSize: 24,
+    color: "#344055",
+    fontWeight: "500",
+    paddingTop: 20,
+    paddingBottom: 15,
+    fontFamily: "Nunito_700Bold",
+    textTransform: "capitalize",
+  },
+  description: {
+    fontSize: 18,
+    color: "#7d7d7d",
+    paddingBottom: 20,
+    fontFamily: "WorkSans_400Regular",
+    lineHeight: 25,
+  },
+
+  inputContainer: {
+    marginTop: 20,
+  },
+  labels: {
+    // fontWeight: "bold",
+    fontSize: 15,
+    color: "#7d7d7d",
+    paddingBottom: 5,
+    fontFamily: "WorkSans_400Regular",
+    lineHeight: 25,
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.3)",
+    paddingHorizontal: 15,
+    paddingVertical: 6,
+    borderRadius: 2,
+  },
+  multiineStyle: {
+    paddingVertical: 4,
+  },
+  buttonStyle: {
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  buttonText: {
+    color: "#eee",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 20,
+    fontFamily: "WorkSans_400Regular",
+  },
+  wrapperText: {
+    marginLeft: 10,
+    color: "#7d7d7d",
+    fontFamily: "WorkSans_400Regular",
+  },
+})
+
+export default Contact
+
